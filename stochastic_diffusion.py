f88f9d135a86d7232c6ada4c48641c748e82411d
ramcdougal <robert.mcdougal@yale.edu>
2020-11-05 15:19:19 -0500

    Add files via upload

--- stochastic_diffusion.py
+++ stochastic_diffusion.py
@@ -1,47 +1,44 @@
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 import time
-try:
-	from randomstate.prng.pcg64 import RandomState
-except ImportError:
-	print """Importing randomstate failed. To fix, try:
-	pip install randomstate"""
-	import sys
-	sys.exit()
-
-random_seed = 1
-random_stream = 1
-prng = RandomState(random_seed, random_stream)
+import random
+import plotnine as p9
+import pandas as pd
 
 x_lo = 0
 x_hi = 200
-num_molecules = 10000
+num_molecules = 100_000
 molecule_locations = [100] * num_molecules
 
 # our rule for diffusion
 def advance(molecules):
-	for i, mol_loc in enumerate(molecules):
-		r = prng.uniform()
-		if r < 0.4:
-			molecules[i] = max(x_lo, mol_loc - 1)
-		elif r < 0.8:
-			molecules[i] = min(x_hi, mol_loc + 1)
+    for i, mol_loc in enumerate(molecules):
+        r = random.random()
+        if r < 0.4:
+            molecules[i] = max(x_lo, mol_loc - 1)
+        elif r < 0.8:
+            molecules[i] = min(x_hi, mol_loc + 1)
 
-def plot_it(molecules, label=''):
-	counts = [0] * (x_hi - x_lo + 1)
-	for mol_loc in molecules:
-		counts[mol_loc] += 1
-	pyplot.plot(counts, label=label)
+def plot_it(molecules, t):
+    counts = [0] * (x_hi - x_lo + 1)
+    for mol_loc in molecules:
+        counts[mol_loc] += 1
+    data = pd.DataFrame({
+            'x': range(x_lo, x_hi + 1),
+            't': t,
+            'count': counts
+    })
+    return p9.geom_line(data=data, size=1)
 
 if __name__ == '__main__':
-	start = time.time()
-	for t in range(2001):
-		if t % 400 == 0:
-			plot_it(molecule_locations, 't = %g' % t)
-		advance(molecule_locations)
-	stop = time.time()
+    my_plot = p9.ggplot(p9.aes(x='x', y='count', color='t'))
+    start = time.perf_counter()
+    for t in range(2001):
+        if t % 400 == 0:
+            my_plot += plot_it(molecule_locations, f'{t} ms')
+        advance(molecule_locations)
+    stop = time.perf_counter()
 
-	print 'simulation time:', stop - start
-
-	pyplot.legend()
-	pyplot.ylim([0, 300])
-	pyplot.show()
+    print(f'simulation time: {stop - start}')
+    my_plot += p9.scale_y_continuous(limits=(0, 3_000))
+    my_plot.draw()
+    plt.show()
